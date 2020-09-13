72a54ef242e00eb5afc2e2ad7af2dd3689ee2298
mboultoureau <contact@mboultoureau.com>
2020-09-13 16:07:12 +0200

    Ajout de l'authentification

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # TakeFood
 
 TakeFood est une application factice simulant la commande de plats à emporter.
+
+## Installation
+
+-   Configurer la connexion à la base de données dans le fichier `.env`.
+-   Écrire les commandes suivantes dans le terminal :
+
+```sh
+composer install
+php bin/console doctrine:migrations:migrate
+
+# Pour initialiser des données de tests
+php bin/console doctrine:fixtures:load
+
+symfony server:start
+```
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=App\Repository\UtilisateurRepository::class)
+ * @UniqueEntity(
+ *  fields={"email"},
+ *  message="L'email renseigné est déjà utilisé."
+ * )
+ */
+class Utilisateur implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=8, minMessage="Votre mot de passe doit faire au moins {{ limit }} caractères.")
+     */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Votre mot de passe est différent de celui de confirmation.")
+     */
+    private $confirmPassword;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getConfirmPassword(): ?string
+    {
+        return $this->confirmPassword;
+    }
+
+    public function setConfirmPassword(string $confirmPassword): self
+    {
+        $this->confirmPassword = $confirmPassword;
+
+        return $this;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+}
