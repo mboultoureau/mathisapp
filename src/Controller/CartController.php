150edd32f7babadca4a95d46dee2ed3316881d44
mboultoureau <contact@mboultoureau.com>
2020-09-27 20:33:48 +0200

    Ajout d'un panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,18 +2,47 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="cart")
+     * @Route("/panier", name="cart.view")
      */
-    public function index()
+    public function index(SessionInterface $session, ProductRepository $productRepository)
     {
+        $cart = $session->get('cart', []);
+
+        $cartWithData = [];
+        foreach ($cart as $id => $quantity) {
+            $cartWithData[] = [
+                'product' => $productRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'cart' => $cartWithData
         ]);
+    }
+
+    /**
+     * @Route("/panier/ajouter/{id}", name="cart.add")
+     */
+    public function add($id, SessionInterface $session)
+    {
+        $cart = $session->get('cart', []);
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $session->set('cart', $cart);
+
+        dd($session->get('cart'));
     }
 }
